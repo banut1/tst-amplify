0936377a4bfcbd68f72e6cefd6d5ff1995d82838
banut1 <97966138+banut1@users.noreply.github.com>
2022-07-05 16:31:48 +0300

    rm searchable

--- amplify/hooks/post-remove-env.js
+++ amplify/hooks/post-remove-env.js
@@ -4,7 +4,7 @@ import {getEnvName, getParameters} from "./utils.js";
 const hookHandler = async (data) => {
   const env = getEnvName(data);
   console.log(`Removing cdk main stack for env ${env}`);
-  execSync(`cdk destroy tstteo-${env}`, { stdio: 'inherit' });
+  execSync(`pwd && cd cdk && npx cdk destroy tstteo-${env} --force`, { stdio: 'inherit' });
   console.log('Removed!');
 };
 
